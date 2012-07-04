f03f1d845b06c6c08d4e6a686954f296f169ac8a
Martin Häger <martin.hager@minmb.se>
2012-07-04 13:53:47 +0200

    ssh-key: refactoring and support for multiple users

--- easy2/common.sh
+++ easy2/common.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+# -*- mode: bash; tab-width: 4; indent-tabs-mode: nil -*-
+
 EC2_META_API_ENDPOINT=http://169.254.169.254/1.0/meta-data
 
 function easy2-instance-meta() {
 
-  local key value
+    local key value
 
-  key=$1
-  wget --timeout=0 -qO- $EC2_META_API_ENDPOINT/$key 2>/dev/null
+    key=$1
+    wget --timeout=0 -qO- $EC2_META_API_ENDPOINT/$key 2>/dev/null
 
-  return $?
+    return $?
 }
 
 function easy2-info() {
-  logger -t easy2 -p syslog.info $1
+    logger -t easy2 -p syslog.info $1
 }
 
 function easy2-error() {
-  logger -t easy2 -p syslog.error $1
+    logger -t easy2 -p syslog.error $1
 }
